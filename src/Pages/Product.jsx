c1a222408a45231864a804bf563e35d326cfd272
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-08-11 19:59:03 +0600

    Add RelatedProducts component with styling and integrate into Product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -5,6 +5,7 @@ import all_product from './../Components/Assets/all_product';
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
   const {all_product}= useContext(ShopContext);
@@ -16,6 +17,7 @@ const Product = () => {
       <Breadcrum product = {product}/>
       <ProductDisplay product={product} />
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
