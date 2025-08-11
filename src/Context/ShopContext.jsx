5f90b8f1438baf2120e30cec00f0684b49aa83fe
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-08-11 19:43:36 +0600

    Add ProductDisplay component with styling and integrate with ShopContext for cart functionality

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,17 +1,59 @@
-import React, { createContext } from 'react'
-import all_product from './../Components/Assets/all_product';
-
+import React, { createContext, useState } from "react";
+import all_product from "./../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
- const ShopContextProvider = (props) => {
-  const contextValue={all_product};
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }));
+    console.log("Added to cart:", itemId);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        if (itemInfo) {
+          totalAmount += itemInfo.new_price * cartItems[item];
+        }
+      }
+    }
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
-      </ShopContext.Provider>
-  )
-
-}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
